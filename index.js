d6b5184c62a245cccba2a683e5aa13d84ca46075
Jarrian McLean <jarrian.mclean@gmail.com>
2018-08-24 07:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 var you = happyHolidaysTo(name)
-    
+    Independence Day = happyHolidaysTo(holiday)
+    20 = holidayCountdown(days)
 function happyHolidays(){
   return "Happy holidays!"
 }
