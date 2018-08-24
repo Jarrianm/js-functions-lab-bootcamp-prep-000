9587a5fa5cfdf1a898d7b06d6e6697a9fd405e79
Jarrian McLean <jarrian.mclean@gmail.com>
2018-08-24 04:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,4 @@ function happyHolidays(){
 
 function happyHolidaysTo(you){
   return "Happy holidays, ${you}!"
-}
 
-function happyHolidayTo(Independence Day, you){
-  return 'Happy ${Independence Day}, ${you}!'
-}
