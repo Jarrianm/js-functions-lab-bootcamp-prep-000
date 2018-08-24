f445575156b10968a46acb65c1e998c6d64ee29c
Jarrian McLean <jarrian.mclean@gmail.com>
2018-08-24 08:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-// function happyHolidayTo(holiday, name) {
-//   return "Happy ${holiday}, ${name}!"
-// }
-// function holidayCountdown(holiday, days) {
-//   return "It/'s ${days} days until ${holiday}!"
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, days) {
+  return `It/'s ${days} days until ${holiday}!`
