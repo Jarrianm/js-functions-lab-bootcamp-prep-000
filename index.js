4a6984317982d596f969e951912dc67e73d9f7f6
Jarrian McLean <jarrian.mclean@gmail.com>
2018-08-24 04:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 var ${name}! = "Jarrian"
-function happyHolidaysTo(Jarrian){
+function happyHolidaysTo(string){
   return "Happy holidays, ${name}!"
 }
